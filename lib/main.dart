01af4bc971923e0601047fed852c07fbffe680e8
vinojanuar <vinojanuar839@gmail.com>
2025-06-15 18:13:48 -0700

    tugas13

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tugass_fluterr/projectlatihan/pagess/add_pages.dart';
+import 'package:tugass_fluterr/tugas13/screen/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Aplikasi Siswa',
       theme: ThemeData(primarySwatch: Colors.pink),
-      home: const AddPages(),
+      home: const LoginScreen(),
     );
   }
 }
